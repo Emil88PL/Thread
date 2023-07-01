5ed70f03b10698350faadb00b0c5360057b91c10
Emil <emil88pl@gmail.com>
2023-07-01 13:52:16 +0100

    MultiThreading

--- src/App.java
+++ src/App.java
@@ -1,27 +1,19 @@
 public class App {
     public static void main(String[] args) throws InterruptedException {
         
-                                                    // Have to change to JDK 14 or JDK 17 as JDK 20 threading did not work!!!!!!!! 
-     //   System.out.println(Thread.activeCount());  // No idea why Thread.activeCount doesn't count correctly with out that line
-        //Thread.currentThread().setName("First thread");
-        //System.out.println(Thread.currentThread().getName());
-
-     //   Thread.currentThread().setPriority(1);
-        // System.out.println(Thread.currentThread().getPriority()); 
-
-        // System.out.println(Thread.currentThread().isAlive());
-
-        MyThread thread2 = new MyThread();
+    
         
-       // thread2.setDaemon(true); 
-       // System.out.println(thread2.isDaemon());
-        thread2.start(); // start Thread not run method from the MyThread
+        MyThread thread1 = new MyThread(); // 1 way
 
-        // System.out.println(thread2.isAlive());
-        // thread2.setName("Second thread");
-        // System.out.println(thread2.getName());
-      //   thread2.setPriority(6);
-         //System.out.println(thread2.getPriority()); // inherited from parent
+        MyRunnable runnable1 = new MyRunnable(); //
+        Thread thread2 = new Thread(runnable1); // 2 way
+
+
+
+
+        thread1.start(); 
+        thread2.start(); 
+
 
         System.out.println(Thread.activeCount());
     }
--- src/MyRunnable.java
+++ src/MyRunnable.java
@@ -1,9 +1,9 @@
-public class MyThread extends Thread { // 1 way
-    
+public class MyRunnable implements Runnable {  // 2 way
+
     @Override
     public void run() {
-        for (int i = 10; i > 0 ; i--) {
-            System.out.println("Thread #1 " + i);
+         for (int i = 0; i < 10 ; i++) {
+            System.out.println("Thread #2 " + i);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -11,6 +11,8 @@ public class MyThread extends Thread { // 1 way
                 e.printStackTrace();
             }
         }
-        System.out.println("Thread #1 is finished");
+        System.out.println("Thread #2 is finished");
     }
+    
+
 }
